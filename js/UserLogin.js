04d8ecbde86047e603c1d2e4a65ba1299d5fdc22
Joy-Ese <86070579+Joy-Ese@users.noreply.github.com>
2022-10-23 10:05:53 +0100

    Update --
    * Validation has been added to the frontend.
    * The end point to register a new user has been successfully added to the frontend.
    * The login end point with bearer token has been added to the frontend. Upon successful login another end point is called on the same fetch request to retrieve user details if token is not null.

--- js/UserLogin.js
+++ js/UserLogin.js
@@ -13,24 +13,33 @@ loginForm.addEventListener("submit", function (e) {
   var myHeaders = new Headers();
   var payloadData = sendLoginData();
   myHeaders.append("Content-Type", "application/json");
-  fetch("https://localhost:7236/api/User/login", {
+  fetch("https://localhost:44378/api/User/login", {
     method: "POST",
     headers: myHeaders,
     body: JSON.stringify(payloadData),
-  }).then(response => {
-    const getUserDetails = () => {
-      var myHeaders = new Headers();
-      myHeaders.append("Authorization", `Bearer ${response}`);
-      fetch("https://localhost:7236/api/Dashboard/user-details", {
-      method: "GET",
-      headers: myHeaders,
-      }).then(response => response.json())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-      getUserDetails();
-    }
-  })
-  .then(result => console.log(result))
+  }).then(response => response.text())
+  .then(response => 
+    {
+      if (response != null) 
+      {
+        const getUserDetails = () => {
+          var myHeaders = new Headers();
+          myHeaders.append("Authorization", `Bearer ${response}`);
+          fetch("https://localhost:44378/api/Dashboard/user-details", {
+          method: "GET",
+          headers: myHeaders,
+          })
+          .then(responseData => responseData.json())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error))
+        }
+        getUserDetails();
+      }
+      else {
+        console.log("Check Login Again")
+      }
+    })
+  // .then(result => console.log(result))
   // .then(() => {window.setTimeout(function(){location.reload()},3000)})
   .catch(error => console.log('error', error));
 });
--- js/UserSignUp.js
+++ js/UserSignUp.js
@@ -28,3 +28,145 @@ signUpForm.addEventListener("submit", function (e) {
   .catch(error => console.log('error', error));
 });
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Validate Sign Up Form
+  const form = document.getElementById("signUpForm");
+  const firstname = document.getElementById("firstName");
+  const lastname = document.getElementById("lastName");
+  const username = document.getElementById("userName");
+  const password = document.getElementById("password");
+  const email = document.getElementById("email");
+  const phonenum = document.getElementById("phoneNumber");
+  const addr = document.getElementById("address");
+
+  signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    Validate();
+  })
+
+
+  const sendData = (firstnameVal, sRate, Count) => {
+    if(sRate === Count){
+        swal("Hello " + firstnameVal , "You have been registered successfully", "success");
+    }
+  }
+
+  const SuccessMsg = (firstnameVal) => {
+    let formContr = document.getElementsByClassName('form-outline');
+    var Count = formContr.length - 1;
+    for(var i = 0; i < formContr.length; i++){
+      if(formContr[i].className === "form-outline success"){
+        var sRate = 0 + i;
+        console.log(sRate);
+        sendData(firstnameVal, sRate, Count);
+      }
+      else{
+        return false;
+      }
+    }
+  }
+
+function Validate() {
+    const firstnameVal = firstname.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const usernameVal = username.value.trim();
+    const passwordVal = password.value.trim();
+    const emailVal = email.value.trim();
+    const phonenumVal = phonenum.value.trim();
+    const addrVal = addr.value.trim();
+
+  // First name
+  if (firstnameVal === "") {
+    setErrorMsg(firstname, "First name required");
+  }
+  else if (firstnameVal.length <= 2) {
+    setErrorMsg(firstname, "Minimum of 3 characters");
+  }
+  else {
+    setSuccessMsg(firstname);
+  }
+
+  // Last name
+  if (lastnameVal === "") {
+    setErrorMsg(lastname, "Last name required");
+  }
+  else if (lastnameVal.length <= 2) {
+    setErrorMsg(lastname, "Minimum of 3 characters");
+  }
+  else {
+    setSuccessMsg(lastname);
+  }
+
+  // Username
+  if (usernameVal === "") {
+    setErrorMsg(username, "Last name required");
+  }
+  else if (usernameVal.length <= 2) {
+    setErrorMsg(username, "Minimum of 3 characters");
+  }
+  else {
+    setSuccessMsg(username);
+  }
+
+  // Password
+  if (passwordVal === "") {
+    setErrorMsg(password, "Password required");
+  }
+  else if (passwordVal.length <= 7) {
+    setErrorMsg(password, 
+      "Min eight char, one uppercase, one lowercase and one number");
+  }
+  else {
+    setSuccessMsg(password);
+  }
+
+  // Email
+  if (emailVal === "") {
+    setErrorMsg(email, "Email required");
+  }
+  else if (emailVal.length <= 5) {
+    setErrorMsg(email, "Invalid email address");
+  }
+  else {
+    setSuccessMsg(email);
+  }
+
+  // Phone number
+  if (phonenumVal === "") {
+    setErrorMsg(phonenum, "Phone number required");
+  }
+  else if (phonenumVal.length <= 10) {
+    setErrorMsg(phonenum, "Invalid phone number");
+  }
+  else {
+    setSuccessMsg(phonenum);
+  }
+
+  // Address
+  if (addrVal === "") {
+    setErrorMsg(addr, "Last name required");
+  }
+  else if (addrVal.length <= 2) {
+    setErrorMsg(addr, "Minimum of 3 characters");
+  }
+  else {
+    setSuccessMsg(addr);
+  }
+
+  SuccessMsg(firstnameVal);
+}
+
+function setErrorMsg (input, errormsgs) {
+  const formOut = input.parentElement;
+  const p = formOut.querySelector('p');
+  formOut.className = "form-outline error";
+  p.innerText = errormsgs;
+}
+
+function setSuccessMsg (input) {
+  const formOut = input.parentElement;
+  formOut.className = "form-outline success";
+}
+
+
